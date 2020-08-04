85be56e3f98e9f88b06b63df61095243af5bb6f5
nbigaud <nathan.bigaud@dalberg.com>
2020-08-04 19:23:12 +0200

    Modify path to engine to fit dropbox setup

--- src/engine.py
+++ src/engine.py
@@ -1,8 +1,9 @@
 import json
 
+
 def get_engine():
-    default_engine_json = '../config/engine.json'
+    default_engine_json = 'config/engine.json'
     with open(default_engine_json) as f:
-        ENGINE = dict((p['identifier'], p['value']) for section in json.load(f) 
-                                                    for p in section['engineParameters'])
+        ENGINE = dict((p['identifier'], p['value']) for section in json.load(f)
+                      for p in section['engineParameters'])
         return ENGINE
