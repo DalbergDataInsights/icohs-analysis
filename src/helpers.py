dcfa740a79003fc64a940233cfe8f3fb1966147a
nbigaud <nathan.bigaud@dalberg.com>
2020-08-13 20:59:27 +0200

    Further simplified teh function to correct error

--- src/helpers.py
+++ src/helpers.py
@@ -1,15 +1,13 @@
 import json
 from datetime import datetime
 
-INDICATORS = parse_config()
 
 def parse_config(config_path='config/indicators.json', config_section='data'):
 
-    for path in config_path:
-        with open(path) as f:
-            ENGINE =   {p['identifier']: p['value']
-                       for section in json.load(f)
-                       for p in section[config_section]}
+    with open(config_path) as f:
+        ENGINE = {p['identifier']: p['value']
+                  for section in json.load(f)
+                  for p in section[config_section]}
 
     return ENGINE
 
@@ -17,3 +15,5 @@ def parse_config(config_path='config/indicators.json', config_section='data'):
 def make_note(statement, start_time):
     print(statement, str(datetime.now() - start_time))
 
+
+INDICATORS = parse_config()
