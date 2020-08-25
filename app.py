78c67ffc9d3ab43610a81d234a4f08b013d46c1d
nbigaud <nathan.bigaud@dalberg.com>
2020-08-25 20:09:44 +0200

    App.py : update to account for new structure of the directory and functions
    Using all data in teh 'raw' folder as input. WIP, not yet tested.

--- app.py
+++ app.py
@@ -18,27 +18,12 @@ if __name__ == '__main__':
     db.pg_write_lookup(INDICATORS['name_district_map'], 'location')
 
     # cleaning the data
-    clean_data = clean.clean(INDICATORS['new_instance_data'],
-                             INDICATORS['old_instance_data'],
-                             INDICATORS['new_instance_data_report'],
-                             INDICATORS['old_instance_data_report'])
+    files = os.listdir(INDICATORS['raw_data'])
 
-    pd.DataFrame(clean_data['dataElement'].unique())\
-        .to_csv('data/temp/indicators.csv')
-    db.pg_write_lookup('data/temp/indicators.csv', 'indicator')
+    for f in files:
+        clean.clean(INDICATORS['raw_data']+f)
 
-    indicator_map = db.pg_read_lookup('indicator')
-    clean_data['dataElement'] = clean_data['dataElement'].map(indicator_map)
-
-    make_note('Backing up csv', START_TIME)
-
-    # order of clean csv file [district, facility, indicator, year, month , value]
-
-    clean_data[['orgUnit', 'dataElement', 'year', 'month', 'value']].to_csv(
-        'data/temp/data_clean.csv', index=False, header=False)
-    # del clean_data
-    make_note('Writing to the database', START_TIME)
-    db.pg_write_table('data/temp/data_clean.csv', 'repository')
+    # Processing the data
 
     reporting, outlier_data, std_no_outlier_data, iqr_no_outlier_data = process.process(
         db.pg_read_table_by_indicator())
