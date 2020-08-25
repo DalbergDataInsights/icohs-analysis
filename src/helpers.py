e5e0754e1026c3a489db291642e70f89b706f278
Nancy Lubalo <nancyatienno@gmail.com>
2020-08-25 18:30:57 +0300

    fix json helper function

--- src/helpers.py
+++ src/helpers.py
@@ -5,7 +5,23 @@ from datetime import datetime
 def parse_config(config_path='config/indicators.json', config_section=['input', 'static', 'to_classify', 'output']):
 
     with open(config_path) as f:
-        for x in config_section:
+      
+            ENGINE = {p['identifier']: p['value']
+                      for section in json.load(f)
+                      for x in config_section
+                      for p in section[x]}
+
+    return ENGINE
+
+
+def parse_config1(config_path='config/indicators.json', config_section=['input', 'static', 'to_classify', 'output']):
+
+    with open(config_path) as f:
+            for section in json.load(f):
+              for x in config_section:
+                 print(section[x])
+                
+        
             ENGINE = {p['identifier']: p['value']
                       for section in json.load(f)
                       for p in section[x]}
