2350111348d9b8f47cd96ff3d3e2e5d4f0cdc178
nbigaud <nathan.bigaud@dalberg.com>
2020-08-26 13:24:43 +0200

    App.py : decoupled lookuptable from the cleaning process (now done month by month) and solved dotenv import bug

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 
+from src.pipeline import clean, process
+from src.helpers import INDICATORS, make_note
+import os
+import pandas as pd
 from datetime import datetime
 
-from src.helpers import INDICATORS, make_note
-from src.pipeline import clean, process
-from src.db import adpter as db
-import pandas as pd
-from dotenv import load_dotenv, find_dotenv
+from dotenv import load_dotenv, find_dotenv  # NOQA: E402
+load_dotenv(find_dotenv(), verbose=True)  # NOQA: E402
+from src.db import adpter as db  # NOQA: E402
 
 START_TIME = datetime.now()
+VAR_CORR = pd.read_csv(INDICATORS['var_correspondence_data'])
 
 if __name__ == '__main__':
 
     make_note('Starting the pipeline', START_TIME)
 
-    load_dotenv(find_dotenv())
-
     db.pg_write_lookup(INDICATORS['name_district_map'], 'location')
+
+    pd.DataFrame(VAR_CORR['identifier'].unique())\
+        .to_csv('data/temp/indicators')
+    db.pg_write_lookup('data/temp/', 'indicator')
 
     # cleaning the data
     files = os.listdir(INDICATORS['raw_data'])
 
     for f in files:
-        clean.clean(INDICATORS['raw_data']+f, INDICATORS['processed_data'])
+        clean.clean(INDICATORS['raw_data']+f, INDICATORS['processed_data']+f)
         make_note(f'Cleaning done for file {f}', START_TIME)
 
     # Processing the data
 
-    reporting, outlier_data, std_no_outlier_data, iqr_no_outlier_data = process.process(
-        db.pg_read_table_by_indicator())
+    # reporting, outlier_data, std_no_outlier_data, iqr_no_outlier_data = process.process(
+        # db.pg_read_table_by_indicator())
 
     # recording measured time
     make_note('Pipeline done', START_TIME)
