dd760b56084a39901295b195ab4ec6df6f9d5e00
nbigaud <nathan.bigaud@dalberg.com>
2020-08-27 19:11:53 +0200

    Helper : automatically linted as I saved changes, no substancial changes

--- src/helpers.py
+++ src/helpers.py
@@ -5,11 +5,11 @@ from datetime import datetime
 def parse_config(config_path='config/indicators.json', config_section=['input', 'static', 'to_classify', 'output']):
 
     with open(config_path) as f:
-      
-            ENGINE = {p['identifier']: p['value']
-                      for section in json.load(f)
-                      for x in config_section
-                      for p in section[x]}
+
+        ENGINE = {p['identifier']: p['value']
+                  for section in json.load(f)
+                  for x in config_section
+                  for p in section[x]}
 
     return ENGINE
 
@@ -17,14 +17,13 @@ def parse_config(config_path='config/indicators.json', config_section=['input', 
 def parse_config1(config_path='config/indicators.json', config_section=['input', 'static', 'to_classify', 'output']):
 
     with open(config_path) as f:
-            for section in json.load(f):
-              for x in config_section:
-                 print(section[x])
-                
-        
-            ENGINE = {p['identifier']: p['value']
-                      for section in json.load(f)
-                      for p in section[x]}
+        for section in json.load(f):
+            for x in config_section:
+                print(section[x])
+
+        ENGINE = {p['identifier']: p['value']
+                  for section in json.load(f)
+                  for p in section[x]}
 
     return ENGINE
 
