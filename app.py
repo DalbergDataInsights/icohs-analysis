03a73f84df5367c7e9b92d1ecaf87337fc6e276f
nbigaud <nathan.bigaud@dalberg.com>
2020-08-27 23:56:33 +0200

    App : updated the writing of the clean data to database, the overall structure, and added writing of the final table to the database

--- app.py
+++ app.py
@@ -12,11 +12,12 @@ from src.db import adpter as db  # NOQA: E402
 
 START_TIME = datetime.now()
 VAR_CORR = pd.read_csv(INDICATORS['var_correspondence_data'])
-FACILITY_IDS = pd.read_csv(INDICATORS['name_district_map'])
 
 if __name__ == '__main__':
 
     make_note('Starting the pipeline', START_TIME)
+
+    # Setting  up the lookup table in teh database
 
     pd.DataFrame(VAR_CORR['identifier'].unique())\
         .to_csv('data/temp/indicators.csv')
@@ -24,17 +25,57 @@ if __name__ == '__main__':
 
     db.pg_write_lookup(INDICATORS['name_district_map'], 'location')
 
-    # cleaning the data
+    # TODO: Making sure the data we need is here with an assert
+    # Checking that both a reporting and a main data files are there
+
     files = os.listdir(INDICATORS['raw_data'])
 
+    # cleaning the data and writing it to the database file by file
+
     for f in files:
-        clean.clean(INDICATORS['raw_data']+f, INDICATORS['processed_data']+f)
-        make_note(f'Cleaning done for file {f}', START_TIME)
+
+        raw_path = INDICATORS['raw_data']+f
+        processed_path = INDICATORS['processed_data']+f
+
+        # Clean the data
+
+        df = clean.clean(raw_path)
+
+        # Send it to a temporary csv
+
+        (temp_csv_path,
+         year,
+         month,
+         table) = clean.map_to_temp(raw_path,
+                                    db.pg_read_lookup('indicator'),
+                                    df)
+
+        # Write the clean data to the database
+
+        db.pg_update_write(year, month, temp_csv_path, table)
+
+        # Move orginal data from the 'raw' to the 'prcessed' folder
+
+        # TODO : commented out for testing purposes
+        clean.move_csv_files(raw_path, processed_path)
+
+        make_note(f'Cleaning and database insertion done for file {f}',
+                  START_TIME)
 
     # Processing the data
 
-    reporting, outlier_data, std_no_outlier_data, iqr_no_outlier_data = process.process(
-        db.pg_read_table_by_indicator())
+    process.process(db.pg_read_table_by_indicator('main'),
+                    db.pg_read_table_by_indicator('report'))
+
+    # Writing to the database
+    db.pg_final_table(INDICATORS['report_data'],
+                      'report_output')
+    db.pg_final_table(INDICATORS['outlier_data'],
+                      'outlier_output')
+    db.pg_final_table(INDICATORS['std_no_outlier_data'],
+                      'std_no_outlier_output')
+    db.pg_final_table(INDICATORS['iqr_no_outlier_data'],
+                      'iqr_no_outlier_output')
 
     # recording measured time
     make_note('Pipeline done', START_TIME)
