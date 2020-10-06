7411af2ad8ce494516bee6de1e477bb7b557e7fa
nbigaud <nathan.bigaud@dalberg.com>
2020-10-06 17:56:41 +0200

    draft of pop function

--- tester.py
+++ tester.py
@@ -4,28 +4,34 @@ from src.helpers import INDICATORS
 VAR_CORR = pd.read_csv(INDICATORS['var_correspondence_data'])
 
 
-def create_indic_map(path):
+def clean_pop_to_temp(pop_path, pop_perc_path):
 
-    indics = []
+    pop = pd.read_csv(pop_path)
 
-    VAR_CORR['breakdown'] = VAR_CORR['breakdown'].astype(str)
+    district_name_dict = {'SEMBABULE': 'SSEMBABULE',
+                          'MADI-OKOLLO': 'MADI OKOLLO', 'LUWEERO': 'LUWERO'}
+    pop['District'] = pop['District'].apply(lambda x: x.upper())
+    pop['District'].replace(district_name_dict, inplace=True)
 
-    for i in VAR_CORR.index:
-        suf = list(VAR_CORR.loc[i, 'breakdown'].split(","))
-        iden = VAR_CORR.loc[i, 'identifier']
-        if suf[0] != 'nan':
-            for s in suf:
-                y = iden + "__" + s
-                indics.append(y)
-        else:
-            y = iden
-            indics.append(y)
+    pop['Age'] = pop['Single Years'].apply(
+        lambda x: ' '.join(x.split(' ')[:1]))
+    pop['Age'].replace({'80+': '80'}, inplace=True)
+    pop['Age'] = pop['Age'].astype('int')
 
-    df = pd.DataFrame(set(indics))
+    pop.drop(['Single Years', 'Year2', 'FY'], axis=1, inplace=True)
 
-    return df
+    pop = pop.groupby(['District', 'Year'], as_index=False).sum()
+
+    pop = pop[['District', 'Year', 'Male', 'Female', 'Total']]
+
+    perc = pd.read_csv(pop_perc_path).set_index('metric')
+
+    for x in perc.index:
+        pop[x] = pop['Total']*(perc.loc[x, 'percentage']/100)
+
+    pop.to_csv('data/temp/pop.csv', index=False, header=False)
 
 
-test = create_indic_map(INDICATORS['indicators_map'])
+# INDICATORS['pop']
 
-print(test)
+clean_pop_to_temp(INDICATORS['pop'], INDICATORS['pop_perc'])
