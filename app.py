c3c64c803992fd25123ed9304cb71e556481ea57
alexkarari <karari.alexander@gmail.com>
2022-01-10 11:22:25 +0300

    Downloading data from dhis2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from src.pipeline import clean, process, indic
+# from src.pipeline import clean, process, indic
 from src.helpers import INDICATORS, make_note, get_unique_indics, get_engine
 import os
 from datetime import datetime
@@ -8,7 +8,7 @@ from dotenv import load_dotenv, find_dotenv  # NOQA: E402
 
 load_dotenv(find_dotenv(), verbose=True)  # NOQA: E402
 
-from src.db import adpter as db  # NOQA: E402
+# from src.db import adpter as db  # NOQA: E402
 from src.api.ddi_dhis2 import Dhis  # NOQA: E402
 
 START_TIME = datetime.now()
@@ -16,8 +16,8 @@ START_TIME = datetime.now()
 with open(INDICATORS["data_config"], "r", encoding="utf-8") as f:
     VAR_CORR = json.load(f)
 import argparse
-import src.api_main as api
-import src.pipeline_main as pipeline
+# import src.api_main as api
+# import src.pipeline_main as pipeline
 
 commands = {
     "setupdb": "Set up the postgres SQL database on the very first run",
@@ -31,60 +31,64 @@ commands = {
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "-a", "--action", choices=list(commands.keys()), default="latest"
-    )
-    parser.add_argument(
-        "-m", "--months", choices=[str(i) for i in range(1, 25)], default=3
-    )
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument(
+    #     "-a", "--action", choices=list(commands.keys()), default="latest"
+    # )
+    # parser.add_argument(
+    #     "-m", "--months", choices=[str(i) for i in range(1, 25)], default=3
+    # )
 
-    args = parser.parse_args()
+    # args = parser.parse_args()
 
     #Send off to DHIS2
     
     api = Dhis(
         os.environ.get("API_USERNAME"),
         os.environ.get("API_PASSWORD"),
-        "https://repo.hispuganda.org/repo/api",
+        "https://hmis-repo.health.go.ug",
+  
+        
     )
     # downloading from dhis2
     new_instance_dataset_id = [id_ for name, id_ in get_engine("config/data_elements.json", "new_datasetIDs").items()]
-    api.get(new_instance_dataset_id, "startDate", "endDate", rename=True, filename="data.csv", orgUnit=None)
-    api.get_report()
-    # Checking for 'special' commands
+    api.get(new_instance_dataset_id, "Jan 01 2021", "Feb 01 2021", rename=True, filename="data_v2.csv", orgUnit=None)
+    new_instance_report = [id_ for name, id_ in get_engine("config/data_elements.json", "report_new").items()]
+    api.get_report(new_instance_report, "dataSet", filepath=None)
+   
+    # # Checking for 'special' commands
 
-    if args.action == "setupdb":
-        pipeline.db.pg_recreate_tables()
+    # if args.action == "setupdb":
+    #     pipeline.db.pg_recreate_tables()
 
-    # Running the API
+    # # Running the API
 
-    if any(args.action in s for s in ["bulk", "apibulk"]):
-        api.run("new", "bulk", int(args.months))
-        api.run("old", "bulk", int(args.months))
+    # if any(args.action in s for s in ["bulk", "apibulk"]):
+    #     api.run("new", "bulk", int(args.months))
+    #     api.run("old", "bulk", int(args.months))
 
-        df = db.pg_read(output) # read from csv
-        df = indic.transform_for_dhis2(
-            df=df, map=db.pg_read("indicator"), outtype=output[:3]
-        )
-        filepath = f"data/temp/{output}_dhis.csv"
-        df.to_csv(filepath, index=False)
-        make_note(f"Publishing {output} to the DHIS2 repo", START_TIME)
+    #     df = db.pg_read(output) # read from csv
+    #     df = indic.transform_for_dhis2(
+    #         df=df, map=db.pg_read("indicator"), outtype=output[:3]
+    #     )
+    #     filepath = f"data/temp/{output}_dhis.csv"
+    #     df.to_csv(filepath, index=False)
+    #     make_note(f"Publishing {output} to the DHIS2 repo", START_TIME)
 
-        api.post([filepath]) # post to dhis2
-    if any(args.action in s for s in ["latest", "apilatest"]):
-        api.run("new", "current", int(args.months))
+    #     api.post([filepath]) # post to dhis2
+    # if any(args.action in s for s in ["latest", "apilatest"]):
+    #     api.run("new", "current", int(args.months))
 
-    # Checking if files needs to be moved
+    # # Checking if files needs to be moved
 
-    if args.action == "pipelinebulkclean":
-        pipeline.clean.move_csv_files_to_input()
+    # if args.action == "pipelinebulkclean":
+    #     pipeline.clean.move_csv_files_to_input()
 
-    # TODO Also add something that only keeps the files in the three years before
+    # # TODO Also add something that only keeps the files in the three years before
 
-    # Running the pipeline
+    # # Running the pipeline
 
-    if any(
-        args.action in s for s in ["bulk", "latest", "pipeline", "pipelinebulkclean"]
-    ):
-        pipeline.run()
+    # if any(
+    #     args.action in s for s in ["bulk", "latest", "pipeline", "pipelinebulkclean"]
+    # ):
+    #     pipeline.run()
